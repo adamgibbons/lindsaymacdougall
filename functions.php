c152b28ff5fc4bca51f0f09ac3c79cf3cd8c489e
Adam Gibbons <adam.d.gibbons@gmail.com>
2012-12-31 10:07:01 -0500

    builds out first iteration of website

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+
+function lm_social_icons() {
+  ?>
+  <nav class="social-icons">
+    <li><a href="#"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/facebook.png"></a></li>
+    <li><a href="#"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/twitter.png"></a></li>
+    <li><a href="#"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/linkedin.png"></a></li>
+    <li><a href="#"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/youtube.png"></a></li>
+  </nav>
+  <?php
+}
+add_action('thematic_header','lm_social_icons', 2);
+
+
+
+function childtheme_header_imgs() {
+  ?>
+  <div id="header-img">
+    <img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/header-banner.png">
+  </div>
+  <?php
+}
+add_action('thematic_header', 'childtheme_header_imgs', 7);
+
+
+
+
+
+?>
