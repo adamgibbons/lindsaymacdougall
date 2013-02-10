e56051254945fb95fe47a63426fe92e32ee85253
Adam Gibbons <adam.d.gibbons@gmail.com>
2013-02-10 12:00:04 -0500

    reworks social icons

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@
 function lm_social_icons() {
   ?>
   <nav class="social-icons">
-    <li><a href="http://www.facebook.com/lindsaymacdougallbiz"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/facebook.png"></a></li>
-    <li><a href="https://twitter.com/LindzMacDougall"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/twitter.png"></a></li>
-    <li><a href="http://www.linkedin.com/pub/lindsay-macdougall/13/988/a37"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/linkedin.png"></a></li>
-    <li><a href="http://www.youtube.com/channel/UCRpgCtmsEErHSK_SegJ202w?feature=plcp"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/youtube.png"></a></li>
-    <li><a href="http://instagram.com/lindzmacdougall/"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/instagram.png"></a></li>
+    <li><a href="http://www.facebook.com/lindsaymacdougallbiz" title="Facebook"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/facebook.png"></a></li>
+    <li><a href="https://twitter.com/LindzMacDougall" title="Twitter"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/twitter.png"></a></li>
+    <li><a href="http://www.linkedin.com/pub/lindsay-macdougall/13/988/a37" title="Linkedin"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/linkedin.png"></a></li>
+    <li><a href="http://www.youtube.com/channel/UCRpgCtmsEErHSK_SegJ202w?feature=plcp" title="Youtube"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/youtube.png"></a></li>
+    <li><a href="http://instagram.com/lindzmacdougall/" title="Instagram"><img src="<?php echo get_bloginfo('siteurl') ?>/wp-content/themes/lindsaymacdougall/img/instagram.png"></a></li>
   </nav>
   <?php
 }
